3ad26d3150a58defcb6534c5211e07f6d334c208
Volodymyr Tkach <vovatkach2427@gmail.com>
2017-03-14 17:48:05 +0200

    created welcome activity

---
+++ app/src/main/java/com/vovatkach2427gmail/ukrainetravel/A.java
@@ -0,0 +1,8 @@
+package com.vovatkach2427gmail.ukrainetravel;
+
+/**
+ * Created by vovat on 13.03.2017.
+ */
+
+public class A {
+}
--- app/src/main/java/com/vovatkach2427gmail/ukrainetravel/Act/WelcomeAct.java
+++ app/src/main/java/com/vovatkach2427gmail/ukrainetravel/Act/WelcomeAct.java
@@ -1,15 +1,49 @@
 package com.vovatkach2427gmail.ukrainetravel.Act;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.OvershootInterpolator;
+import android.widget.TextView;
 
 import com.vovatkach2427gmail.ukrainetravel.R;
 
 public class WelcomeAct extends AppCompatActivity {
-
+    TextView tvWelcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_act);
+        tvWelcome=(TextView)findViewById(R.id.tvWelcomeName);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Interpolator interpolator=new AccelerateInterpolator();
+        ObjectAnimator animator=ObjectAnimator.ofFloat(tvWelcome, View.ALPHA,0.0f,1.0f);
+        animator.setDuration(5000);
+        animator.setInterpolator(interpolator);
+        tvWelcome.setVisibility(View.VISIBLE);
+        animator.start();
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                Intent goToPriewAct=new Intent(WelcomeAct.this,PriewAct.class);
+                startActivity(goToPriewAct);
+                overridePendingTransition(R.anim.in_left,R.anim.out_right);
+            }
+        });
+
     }
 }
